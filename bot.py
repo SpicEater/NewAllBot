c0cd31c4eeaedac721d7343122eeb82e57024577
SpicEater <er.egor2004@yandex.ru>
2024-07-27 13:05:38 +0300

    version 1.3.1 Добавить человека по сылке

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
+from spicy import remember, encrypt_data, decrypt_data
 import logging
 import asyncio
 import aiohttp
-
-from telegram import Update, helpers, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+import telegram
+import sqlite3 as sql
+import requests
+from telegram import Update, helpers, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, ChatInviteLink
 from telegram.ext import Application, ContextTypes, MessageHandler, filters, CommandHandler, CallbackQueryHandler
 
+## создание логов
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
@@ -12,43 +16,106 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+DB = 'user.db' ##путь к БД
 
-async def add_bot(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
-    # keybord_1 =InlineKeyboardButton(text='Ссылка')
-    # keybord_2 =InlineKeyboardButton(text='Меню', callback_data=start)
-
+async def mute(update: Update, context: ContextTypes.DEFAULT_TYPE): ##оброботчик команды /mute
+    chat_id = update.effective_chat.id
+    user_name = update.effective_user.name
+    user_id = update.effective_user.id
+    con = sql.connect(DB)
+    with con:
+        cur = con.cursor()
+        try:
+            cur.execute(f"SELECT DISTINCT title FROM user WHERE NOT id_user = '{user_id}' or name = '{user_name}';") ##провекрка в БД
+        except:
+            await context.bot.send_message(chat_id, "Не нашел вас ни в одном чате.")
+            return False
+        cur.execute("DELETE FROM user WHERE")
+async def add_bot(update: Update, context: ContextTypes.DEFAULT_TYPE): ##оброботчик команды /add_bot
     url = helpers.create_deep_linked_url('spicyeater_bot', "link", group=True)
-    await update.message.reply_text(
-        'Нажми на кнопку, чтобы добавить бота',
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text='Ссылка', url=url)], [InlineKeyboardButton(text='Меню', callback_data='hellp')]]
-        ),
-    )
+    url += '&admin=post_messages'
+    query = update.callback_query
+    try:
+        await query.edit_message_text(
+            'Нажми на кнопку, чтобы добавить бота',
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text='Ссылка', url=url)], [InlineKeyboardButton(text='Меню', callback_data='hellp')]]))
+    except:
+        await update.message.reply_text(
+            'Нажми на кнопку, чтобы добавить бота',
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text='Ссылка', url=url)], [InlineKeyboardButton(text='Меню', callback_data='hellp')]]))
 
+async def add_people_1(update: Update, context: ContextTypes.DEFAULT_TYPE): ##оброботчик команды /add_people
+    user_id= str(update.effective_user.id)
+    chat_id = update.effective_chat.id
+    user_name = update.effective_user.name
+    con = sql.connect(DB)
 
+    with con:
+        cur = con.cursor()
+        try:
+            cur.execute(f"SELECT DISTINCT title FROM user WHERE NOT id_user = '{user_id}' or name = '{user_name}';")##
+            # print(cur.fetchall())
+        except:
+            await context.bot.send_message(chat_id, "Не нашел вас ни в одном чате. Попробуйте перелогиниться, прописав команду /remember в чате")
+            return False
+        w = []
+        for i in cur.fetchall():
+            w.append(i[0])
+        masage = 'Выберите группу в которую хотите добавить человека'
+        button = [[InlineKeyboardButton(i, callback_data=f'add_people_2 {i}')] for i in w] ##
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    reply_keyboard = [
-        ["Добавить людей", "Удалить уведомления"],
-        ["Добавить бота", "Добавить тег"]
-    ]
-    markup = ReplyKeyboardMarkup(reply_keyboard)
-    await update.message.reply_text('⚙️ Меню ⚙️ \n\n'
-                              '— /add_people - добавить людей в группу 👥\n'
-                              '— /mute - удалить уведомления🔕\n'
-                              '— /add_bot - добавить бота  🤖\n'
-                              '— /add_tag - добавить тег для уведомления 🔔\n',
-                                    reply_markup=markup)
+        mark = InlineKeyboardMarkup(button)
+        try:
+            await update.callback_query.edit_message_text(masage, reply_markup=mark)
+        except:
+            await update.message.reply_text(masage, reply_markup=mark)
 
-async def hellp(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text= '⚙️ Меню ⚙️ \n\n'
-                              '— /add_people - добавить людей в группу 👥\n'
-                              '— /mute - удалить уведомления🔕\n'
-                              '— /add_bot - добавить бота  🤖\n'
-                              '— /add_tag - добавить тег для уведомления 🔔\n')
+    # await update.message.reply_text("Напиши название группы в которую хочешь ")
+async def add_people_2(update: Update, context: ContextTypes.DEFAULT_TYPE): ##оброботчик команды /mute
+    args = update.callback_query.data[13::]
+    args = [k if k != " " else "_" for k in list(args)]
+    args = "".join(args)
+    con = sql.connect(DB)
 
-async def periodic_internet_check(interval_hours):
+    if args:
+        with con:
+            cur = con.cursor()
+            try:
+                cur.execute(f"SELECT DISTINCT id_chat FROM user WHERE title = '{args}';")
+                chat = cur.fetchall()
+            except:
+                await context.bot.send_message(update.effective_chat.id,
+                                               "Для того чтобы пригласить когото нужно чтобы хотябы у одного человека были включены уведомления. Попробуйте перелогиниться, прописав команду /remember в чате")
+    link = context.bot.link + f'?start={args}_{chat[0][0]}'
+    await update.callback_query.edit_message_text(f"Поделитесь этой ссылкой для присоединения других пользователей: {link}", reply_markup =
+                                                  InlineKeyboardMarkup([[InlineKeyboardButton(text='Назад', callback_data='start')]]))
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE): ##оброботчик команды /start
+    args = context.args
+    user = update.effective_user.name
+    user_id = update.effective_user.id
+
+    if args:
+        args = args[0].split('_')
+        chat = args[1]
+        title = args[0]
+        if not remember(user, user_id, chat, title):
+            await context.bot.send_message(update.effective_chat.id,
+                                           "Вы уже состоите в этом чате")
+        return
+    masage = '⚙️ Меню ⚙️ \n\n' \
+             '— /add_people - добавить людей в группу 👥\n'\
+            '— /mute - удалить уведомления🔕\n'\
+            '— /add_bot - добавить бота  🤖\n'\
+            '— /add_tag - добавить тег для уведомления 🔔\n'
+    mark = InlineKeyboardMarkup([[InlineKeyboardButton(text='Добавить людей', callback_data='add_people'), InlineKeyboardButton(text='Удалить уведомления', callback_data='mute')],
+                                   [InlineKeyboardButton(text='Добавить бота', callback_data='add_bot'), InlineKeyboardButton(text='Добавить тег', callback_data='add_tag')]])
+    try:
+        await update.callback_query.edit_message_text(masage,reply_markup= mark)
+    except:
+        await update.message.reply_text(masage, reply_markup=mark)
+
+async def periodic_internet_check(interval_hours): ##запуск команды проверки соеденения
     while True:
         internet_connected = await check_internet_connection()
         if internet_connected:
@@ -59,26 +126,41 @@ async def periodic_internet_check(interval_hours):
         await asyncio.sleep(interval_hours * 3600)
 
 
-async def check_internet_connection():
+async def check_internet_connection(): ##проверка соедениния с интернетом
     try:
         async with aiohttp.ClientSession() as session:
-            async with session.get('http://www.google.com') as response:
+            async with session.get('http://www.google.com') as response: ##обращение к google
                 return response.status == 200
     except aiohttp.ClientError:
         return False
+
 
 def main() -> None:
     application = Application.builder().token("6572779723:AAGvYhji-PdqXZWj72E1lrAUjqOMYT5Tz0E").build()
 
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CallbackQueryHandler(hellp, '^hellp$'))
-    application.add_handler(MessageHandler(filters.Regex('^/add_people|Добавить людей$'), add_bot))
+    application.add_handler(CallbackQueryHandler(start, pattern='^start$'))
+
+    application.add_handler(MessageHandler(filters.Regex('^/add_people|Добавить людей$'), add_people_1))
+    application.add_handler(CallbackQueryHandler(add_people_1, '^add_people$'))
+
+    application.add_handler(CallbackQueryHandler(add_people_2, pattern = 'add_people_2'))
+
     application.add_handler(MessageHandler(filters.Regex('^/mute|Удалить уведомления$'), add_bot))
+    application.add_handler(CallbackQueryHandler(add_bot, '^mute$'))
+
     application.add_handler(MessageHandler(filters.Regex('^/add_bot|Добавить бота$'), add_bot))
+    application.add_handler(CallbackQueryHandler(add_bot, '^add_bot$'))
+
     application.add_handler(MessageHandler(filters.Regex('^/add_tag|Добавить тег$'), add_bot))
+    application.add_handler(CallbackQueryHandler(add_bot, '^add_tag$'))
+
     application.add_handler(MessageHandler(filters.Regex('^/hellp|Меню$'), start))
+    application.add_handler(CallbackQueryHandler(start, '^hellp$'))
+
     loop = asyncio.get_event_loop()
-    loop.create_task(periodic_internet_check(interval_hours=6))
+    # loop.create_task(delete_mess(interval_hours=12)
+    loop.create_task(periodic_internet_check(interval_hours=6)) ##выполнение команды проверки интернета каждые 6 часов
 
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
--- spicy.py
+++ spicy.py
@@ -2,6 +2,7 @@ import sqlite3 as sql
 import logging
 import asyncio
 import aiohttp
+from cryptography.fernet import Fernet
 
 from telegram import Update, Bot
 from telegram.ext import Application, ContextTypes, MessageHandler, filters, CommandHandler
@@ -13,37 +14,57 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-async def remember(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    name = context.effective_user
-
 DB = 'user.db'
 
 async def link(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('Отлично, теперь дай мне права на чтение сообщений и я приступлю к работе')
 
-async def remember(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start_remember(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user.name
     id_user = update.effective_user.id
     chat = update.effective_chat.id
     title = update.effective_chat.title
+    remember(user, id_user, chat, title)
+
+def load_key():
+    return open("secret.key", "rb").read()
+
+def encrypt_data(data):
+    key = load_key()
+    fernet = Fernet(key)
+    encrypted_data = fernet.encrypt(data.encode())
+    return encrypted_data
+
+
+# Расшифровка данных
+def decrypt_data(encrypted_data):
+    key = load_key()
+    fernet = Fernet(key)
+    decrypted_data = fernet.decrypt(encrypted_data).decode()
+    return decrypted_data
+def remember (user, id_user, chat, title):
+    title = encrypt_data(title)
     con = sql.connect(DB)
     with con:
         cur = con.cursor()
         if not title: title = 'BOT'
         try:
-            cur.execute("CREATE TABLE IF NOT EXISTS `user` (`name` STRING, `id_user` INTEGER, `title` STRING, `id_chat` INTEGER, tags STRING)")
+            cur.execute("CREATE TABLE IF NOT EXISTS `user` (`name` STRING, `id_user` INTEGER, `title` STRING, `id_chat` INTEGER, tags STRING, 'admin' INTEGER)")
             cur.execute(f"BEGIN TRANSACTION; ")
             cur.execute(f"SELECT COUNT(*) FROM user WHERE name = '{user}' AND title = '{title}';")
+            if cur.fetchall():
+                return False
             cur.execute(f"INSERT INTO user VALUES ('{user}', {id_user}, '{title}', {chat});")
             cur.execute("COMMIT;")
             con.commit()
             cur.close()
-        except: return
+        except:
+            pass
 
 async def call(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     chat = update.effective_chat.id
-    text = sql.connect(DB).execute(f"SELECT name FROM user WHERE id_chat = {chat};").fetchall()
+    text = sql.connect(DB).execute(f"SELECT name FROM user WHERE id_chat = {chat} and title != 'BOT';").fetchall()
     mas = ''
     for f in text:
         mas += f[0] + " "
@@ -72,9 +93,9 @@ def main() -> None:
     application = Application.builder().token("6941947875:AAFVzXUviZAgmuZmV1p5dQVhSOtaVcuuEr8").build()
 
     application.add_handler(CommandHandler("start", link, filters.Regex('link')))
-    application.add_handler(CommandHandler("remember", remember))
+    application.add_handler(CommandHandler("remember", start_remember))
     application.add_handler(MessageHandler(
-                            filters.Regex("@all|@все|@чурки") | filters.CaptionRegex(r"@all|@все|@чурки"), call))
+                            filters.Regex("@all|@все|@чурки|@гойда") | filters.CaptionRegex(r"@all|@все|@чурки|@гойда"), call))
 
     loop = asyncio.get_event_loop()
     loop.create_task(periodic_internet_check(interval_hours=6))
